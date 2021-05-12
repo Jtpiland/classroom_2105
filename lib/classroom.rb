0cc67eb1bdd7b140a629babfeb5209a606686018
Jacob Piland <piland.jacob@gmail.com>
2021-05-12 16:00:02 -0500

    Make first iteration pass

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,27 @@
 class Classroom
+  attr_reader :subject, :capacity
+  def initialize(subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @classroom = []
+    @upper_case_names
+  end
+
+  def students
+    @classroom
+  end
+
+  def add_student(student)
+    @classroom << student
+  end
+
+
+
+  def yell_at_students
+    students.each do |student|
+      student.upcase
+      @upper_case_names << student
+    end
+  end
+
 end
